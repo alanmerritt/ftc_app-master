87c2d24e49f14273964fa44a2946456f0e078336
alanmerritt <merrittalan1@gmail.com>
2017-10-26 21:56:38 -0600

    Created class for saving data to an external file.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DataLogger.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DataLogger.java
@@ -1,5 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 
+//import java.util.Date;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 /**
  * Created by MerrittAM on 10/21/2017.
  * Used to write data out to a log file.
@@ -7,6 +13,45 @@ package org.firstinspires.ftc.teamcode;
 
 public class DataLogger {
 	
-	private final static String LOG_FILEPATH = "/sdcard/FIRST/Calibration.txt";
+	private String logFilePath;
+	private FileOutputStream fileout;
+	
+	public DataLogger() {
+		
+		logFilePath = "/sdcard/FIRST/Debug/Logs/log.txt";
+		
+		try {
+			
+			fileout = new FileOutputStream(logFilePath, true);
+			
+		} catch(FileNotFoundException ex) {
+			
+		}
+			
+	}
+	
+	public void write(String message) {
+		
+		try {
+			
+			fileout.write(message.getBytes());
+			
+		} catch(IOException ex) {
+			
+		}
+		
+	}
+	
+	public void close() {
+		
+		try {
+			
+			fileout.close();
+			
+		} catch(IOException ex) {
+			
+		}
+		
+	}
 	
 }
