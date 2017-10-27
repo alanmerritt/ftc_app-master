4bc36fe6796ba398605c2e0cd8af40a42e1a8653
alanmerritt <merrittalan1@gmail.com>
2017-10-26 21:57:39 -0600

    Created class for storing code common between all OpModes (motors, servos, etc).

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Robot.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by MerrittAM on 10/26/2017.
+ * Class that contains all of the common robot control
+ * elements (motors, servos, sensors, etc.) between
+ * the different OpModes.
+ */
+
+public class Robot {
+}
