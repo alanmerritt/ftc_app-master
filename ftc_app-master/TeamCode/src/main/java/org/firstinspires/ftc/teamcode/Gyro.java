a4596f3b4bb676cf7e1afb93f08ec2bfb1d84b1c
alanmerritt <merrittalan1@gmail.com>
2017-11-02 22:12:35 -0600

    Worked on gyroscope integration.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Gyro.java
@@ -11,14 +11,20 @@ import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
 /**
  * Created by MerrittAM on 9/30/2017.
+ * Class used for easily accessing the imu
+ * builtin to the Rev Expansion Hub.
+ *
+ * It assumes that the imu is named "imu" in
+ * the robot configuration file.
  */
 
 public class Gyro {
 
     BNO055IMU imu;
-
-
-
+	
+	private double totalYaw = 0;
+	private double lastYaw = 0;
+	
     public Gyro(OpMode opMode) {
 
         BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
@@ -33,11 +39,50 @@ public class Gyro {
         imu.initialize(parameters);
 
     }
-
-    public Orientation getAngles() {
+	
+	/**
+	 * Gets the rotational orientation of the robot.
+	 * @return The rotation of the robot. Angles are in ZYX order.
+	 */
+	public Orientation getOrientation() {
 
         return imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
 
     }
-
+	
+	/**
+	 *
+	 * @return
+	 */
+	public double getYaw() {
+	    
+	    return getOrientation().firstAngle;
+	    
+    }
+    
+    public void updateYaw() {
+	    
+	    double yaw = getYaw();
+	    
+	    double diff = yaw - lastYaw;
+	    
+	    if(diff >= 180) {
+		    diff -= 360;
+	    }
+	    if(diff < -180) {
+		    diff += 360;
+	    }
+	    
+	    totalYaw += diff;
+	    
+	    lastYaw = yaw;
+	    
+    }
+    
+    public double getTotalYaw() {
+	    
+	    return totalYaw;
+	    
+    }
+    
 }
