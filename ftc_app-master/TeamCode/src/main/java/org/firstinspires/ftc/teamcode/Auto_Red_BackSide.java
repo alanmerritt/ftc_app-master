9c6c1cce5dc45adf22356ff42bd87365fa6f33bf
alanmerritt <merrittalan1@gmail.com>
2017-12-07 22:27:09 -0700

    Implemented computer vision.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red_BackSide.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red_BackSide.java
@@ -3,10 +3,9 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.hardware.DcMotor;
 
-import org.firstinspires.ftc.teamcode.Auto;
-
 /**
  * Created by MerrittAM on 11/13/2017.
+ * Red autonomous for the back side (side away from the relic drop zone).
  */
 @Autonomous(name = "Auto_Red_BackSide", group = "Autonomous")
 public class Auto_Red_BackSide extends Auto {
@@ -16,23 +15,37 @@ public class Auto_Red_BackSide extends Auto {
 		
 		initialize();
 		
+		relicTrackables.activate();
+		
 		waitForStart();
+		
+		scanVuMark(1000);
+		
+		sleep(1000);
 		
 		grabAndLift();
 		
 		redKnockOff();
 		
-		moveToFirstColumn();
-		
+		//moveToFirstColumn();
+		firstMove();
+		sleep(500);
+		//Move to the side depending on which VuMark is seen.
+		moveToSide();
+		sleep(500);
+		moveForwardToColumn();
+		sleep(500);
 		lowerAndRelease();
 		
 		backup();
 		
 		sleep(500);
 		
+		//TODO: Rotate to face blocks.
+		
 	}
 	
-	private void moveToFirstColumn() {
+	private void firstMove() {
 		
 		//Forward movement.
 		setModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -42,19 +55,38 @@ public class Auto_Red_BackSide extends Auto {
 		while(frontLeft.isBusy() && !isStopRequested());
 		stopDriveMotors();
 		
-		sleep(1000);
+	}
+	
+	private void moveToSide() {
+		
+		int driveDistance;
+		switch(vuMark) {
+			
+			case LEFT:
+				driveDistance = 24;
+				break;
+			case CENTER:
+				driveDistance = 12;
+				break;
+			case RIGHT:
+			default:
+				driveDistance = 5;
+			
+		}
 		
 		setModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 		setModes(DcMotor.RunMode.RUN_TO_POSITION);
-		frontLeft.setTargetPosition(-5 * INCH);
-		frontRight.setTargetPosition(5 * INCH);
-		backRight.setTargetPosition(-5 * INCH);
-		backLeft.setTargetPosition(5 * INCH);
+		frontLeft.setTargetPosition(-driveDistance * INCH);
+		frontRight.setTargetPosition(driveDistance * INCH);
+		backRight.setTargetPosition(-driveDistance * INCH);
+		backLeft.setTargetPosition(driveDistance * INCH);
 		driveMotors(.5, .5, .5, .5);
-		while(frontLeft.isBusy() && !isStopRequested());
+		while (frontLeft.isBusy() && !isStopRequested()) ;
 		stopDriveMotors();
 		
-		sleep(1000);
+	}
+	
+	private void moveForwardToColumn() {
 		
 		//Forward movement.
 		setModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
@@ -63,8 +95,6 @@ public class Auto_Red_BackSide extends Auto {
 		driveMotors(.5, .5, .5, .5);
 		while(frontLeft.isBusy() && !isStopRequested());
 		stopDriveMotors();
-		
-		sleep(1000);
 		
 	}
 	
