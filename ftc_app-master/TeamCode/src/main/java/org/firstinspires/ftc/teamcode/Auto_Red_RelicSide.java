a580fd874e42f2637e1aeedfaef14670a3a63c8d
alanmerritt <merrittalan1@gmail.com>
2017-12-07 22:27:27 -0700

    Implemented computer vision and block placing.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red_RelicSide.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto_Red_RelicSide.java
@@ -14,20 +14,110 @@ public class Auto_Red_RelicSide extends Auto {
 		
 		initialize();
 		
+		relicTrackables.activate();
+		
 		waitForStart();
+		
+		scanVuMark(1000);
+		
+		// --- Knock off balls. ---
 		
 		grabAndLift();
 		
 		redKnockOff();
 		
-		//moveToFirstColumn();
+		// --- Place glyph. ---
+		
+		moveForward();
+		
+		sleep(500);
+		
+		rotate();
+		
+		sleep(500);
+		
+		moveToSide();
+		
+		sleep(500);
+		
+		moveForwardToColumn();
+		
+		sleep(500);
 		
 		lowerAndRelease();
 		
 		sleep(500);
 		
+		backup();
+		
 	}
 	
+	private void moveForward() {
+		
+		setModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		setModes(DcMotor.RunMode.RUN_TO_POSITION);
+		setTargets(22 * INCH);
+		driveMotors(.5, .5, .5, .5);
+		while(frontLeft.isBusy() && !isStopRequested());
+		stopDriveMotors();
+	
+	}
+	
+	private void rotate() {
+		
+		setModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+		double yaw = gyro.getTotalYaw();
+		while(yaw > -85 && !isStopRequested()) {
+			gyro.updateYaw();
+			yaw = gyro.getTotalYaw();
+			driveMotors(.3, -.3, -.3, .3);
+		}
+		stopDriveMotors();
+		
+	}
+	
+	private void moveToSide() {
+		
+		int driveDistance;
+		switch(vuMark) {
+			
+			case LEFT:
+				driveDistance = 14;
+				break;
+			case CENTER:
+				driveDistance = 5;
+				break;
+			case RIGHT:
+			default:
+				driveDistance = -3;
+			
+		}
+		
+		setModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		setModes(DcMotor.RunMode.RUN_TO_POSITION);
+		frontLeft.setTargetPosition(-driveDistance * INCH);
+		frontRight.setTargetPosition(driveDistance * INCH);
+		backRight.setTargetPosition(-driveDistance * INCH);
+		backLeft.setTargetPosition(driveDistance * INCH);
+		driveMotors(.5, .5, .5, .5);
+		while (frontLeft.isBusy() && !isStopRequested()) ;
+		stopDriveMotors();
+		
+	}
+	
+	private void moveForwardToColumn() {
+		
+		//Forward movement.
+		setModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+		setModes(DcMotor.RunMode.RUN_TO_POSITION);
+		setTargets(8 * INCH);
+		driveMotors(.5, .5, .5, .5);
+		while(frontLeft.isBusy() && !isStopRequested());
+		stopDriveMotors();
+		
+	}
+	
+	@Deprecated
 	private void moveToFirstColumn() {
 		
 		telemetry.addLine("Driving forward.");
