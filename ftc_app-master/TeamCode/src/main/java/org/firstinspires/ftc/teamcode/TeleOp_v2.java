238aa3cb051a0a3d8b0042e03c6ef8c664e5e526
alanmerritt <merrittalan1@gmail.com>
2018-01-25 16:31:40 -0700

    Added an auto-grabbing function.

--- ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_v2.java
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOp_v2.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.DigitalChannel;
 import com.qualcomm.robotcore.hardware.Servo;
-import com.qualcomm.robotcore.hardware.TouchSensor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.teamcode.calibration.CalibrationManager;
@@ -52,14 +52,31 @@ public class TeleOp_v2 extends OpMode {
 	private boolean upperGripperPressedLast = false;
 	private boolean upperGripperOn = false;
 	
+	private boolean grabbingStack = false;
 	
+	private enum GRAB_STACK_STATE {
 	
-	private Gyro gyro;
+		RELEASE,
+		TRANSITION_TO_BACKUP,
+		BACKUP,
+		LOWER,
+		FORWARD,
+		GRAB,
+		LIFT
 	
-	private double placementOrientation = 0;
-	private boolean returnToPlacementOrientation = false;
+	}
+	GRAB_STACK_STATE grabState = GRAB_STACK_STATE.RELEASE;
+	int grabStackTarget = (int)(1120*0.25);
+	ElapsedTime timer = new ElapsedTime(ElapsedTime.Resolution.MILLISECONDS);
+	
+//	private Gyro gyro;
+//
+//	private double placementOrientation = 0;
+//	private boolean returnToPlacementOrientation = false;
 	
 	private DigitalChannel liftDetector;
+	
+	
 	
 	@Override
 	public void init() {
@@ -104,7 +121,7 @@ public class TeleOp_v2 extends OpMode {
 		liftDetector.setMode(DigitalChannel.Mode.INPUT);
 		
 		//Initialize the gyroscope.
-		gyro = new Gyro(this);
+//		gyro = new Gyro(this);
 		
 		telemetry.addLine("Hardware mapping complete.");
 		
@@ -127,26 +144,172 @@ public class TeleOp_v2 extends OpMode {
 		liftDownPower = Double.parseDouble(calibrationManager.get("liftPower"));
 		telemetry.addLine("Calibration data loaded.");
 		
+		hardwareMap.servo.get("knockerOffer").setPosition(Double.parseDouble(calibrationManager.get("knockerOfferRaised")));
+		
 		telemetry.update();
 		
 	}
 	
 	@Override
 	public void loop() {
-		
-		gyro.updateYaw();
-		
-		telemetry.addData("Yaw", gyro.getYaw());
-		telemetry.addData("Total yaw", gyro.getTotalYaw());
-		
+//
+//		gyro.updateYaw();
+//
+//		telemetry.addData("Yaw", gyro.getYaw());
+//		telemetry.addData("Total yaw", gyro.getTotalYaw());
+//
 		
 		
 		drive();
 		runLift();
 		runGripper();
 		
+		if(gamepad1.dpad_up) {
+			grabbingStack = true;
+		}
+		
+		if(grabbingStack) {
+			
+			double liftPower = 0;
+			
+			switch(grabState) {
+				
+				case RELEASE:
+					
+					gripperOn = false;
+					upperGripperOn = false;
+					
+					//!!! May need short delay.
+					grabState = GRAB_STACK_STATE.TRANSITION_TO_BACKUP;
+					
+					telemetry.addLine("Releasing.");
+					
+					break;
+					
+				case TRANSITION_TO_BACKUP:
+					
+					setModes(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+					setModes(DcMotor.RunMode.RUN_TO_POSITION);
+					setTargets(grabStackTarget);
+					
+					grabState = GRAB_STACK_STATE.BACKUP;
+					
+					telemetry.addLine("Transition to backup.");
+					
+					break;
+					
+				case BACKUP:
+					
+					if(frontLeft.isBusy()) {
+						setPowers(1);
+					} else {
+						setPowers(0);
+						setTargets(0);
+						grabState = GRAB_STACK_STATE.LOWER;
+					}
+					
+					telemetry.addLine("Backing up.");
+					telemetry.addData("Position", frontLeft.getCurrentPosition());
+					
+					break;
+					
+				case LOWER:
+					
+					if(leftLift.getCurrentPosition() > 200) {
+						liftPower = -.75;
+					} else if(leftLift.getCurrentPosition() > 50) {
+						liftPower = -.4;
+					} else if(leftLift.getCurrentPosition() > 0) {
+						liftPower = -.1;
+					} else {
+						liftPower = 0;
+						grabState = GRAB_STACK_STATE.FORWARD;
+					}
+					
+					leftLift.setPower(liftPower);
+					rightLift.setPower(liftPower);
+					
+					telemetry.addLine("Lowering.");
+					telemetry.addData("Lift position", leftLift.getCurrentPosition());
+					
+					break;
+					
+				case FORWARD:
+					
+					if(frontLeft.isBusy()) {
+						setPowers(1);
+					} else {
+						setPowers(0);
+						timer.reset();
+						grabState = GRAB_STACK_STATE.GRAB;
+					}
+					
+					telemetry.addLine("Moving forward.");
+					telemetry.addData("Position", frontLeft.getCurrentPosition());
+					
+					break;
+				
+				case GRAB:
+					
+					gripperOn = true;
+					upperGripperOn = true;
+					
+					if(timer.time() > 200) {
+						grabState = GRAB_STACK_STATE.LIFT;
+					}
+					
+					telemetry.addLine("Grabbing.");
+					
+					break;
+					
+				case LIFT:
+					
+					if(leftLift.getCurrentPosition() < 150) {
+						liftPower = 1;
+					} else {
+						liftPower = 0;
+					}
+					
+					leftLift.setPower(liftPower);
+					rightLift.setPower(liftPower);
+					
+					telemetry.addLine("Lifting.");
+					telemetry.addData("Lift position", leftLift.getCurrentPosition());
+					
+					break;
+					
+			}
+			
+			
+		} else {
+			grabState = GRAB_STACK_STATE.RELEASE;
+			setModes(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+		}
+		
 		telemetry.update();
 		
+	}
+	
+	
+	private void setModes(DcMotor.RunMode mode) {
+		frontLeft.setMode(mode);
+		frontRight.setMode(mode);
+		backRight.setMode(mode);
+		backLeft.setMode(mode);
+	}
+	
+	private void setTargets(int target) {
+		frontLeft.setTargetPosition(target);
+		frontRight.setTargetPosition(target);
+		backRight.setTargetPosition(target);
+		backLeft.setTargetPosition(target);
+	}
+	
+	private void setPowers(double power) {
+		frontLeft.setPower(power);
+		frontRight.setPower(power);
+		backRight.setPower(power);
+		backLeft.setPower(power);
 	}
 	
 	private void drive() {
@@ -156,66 +319,51 @@ public class TeleOp_v2 extends OpMode {
 		double y = gamepad1.left_stick_y;
 		double r = gamepad1.right_stick_x;
 		
+		if(x != 0 || y != 0 || r != 0) {
+			grabbingStack = false;
+		}
+		
 		telemetry.addData("X", x);
 		telemetry.addData("Y", y);
 		telemetry.addData("R", r);
 		
-		// --- Auto-turning function. ---
-		/*
-		//Rotate toward the box.
-		if(returnToPlacementOrientation) {
-			r = 1;
+		if(!grabbingStack) {
+			//Drive the motors in robot-centric mode.
+			frontLeft.setPower(Range.clip(y - x - r, -1, 1));
+			backLeft.setPower(Range.clip(y + x - r, -1, 1));
+			frontRight.setPower(Range.clip(y + x + r, -1, 1));
+			backRight.setPower(Range.clip(y - x + r, -1, 1));
 		}
-		
-		//Activate the auto-turn.
-		if(gamepad1.y) {
-			returnToPlacementOrientation = true;
-		}
-		
-		//Turning range.
-		double min = placementOrientation - 5;
-		double max = placementOrientation + 5;
-		double position = gyro.getYaw();
-		
-		//If the robot is within the range, stop the function.
-		if(position >= min && position <= max) {
-			returnToPlacementOrientation = false;
-		}
-		
-		telemetry.addData("Returning to placement location", returnToPlacementOrientation);
-		*/
-		// ------------------------------
-		
-		//Drive the motors in robot-centric mode.
-		frontLeft.setPower(Range.clip(y - x - r, -1, 1));
-		backLeft.setPower(Range.clip(y + x - r, -1, 1));
-		frontRight.setPower(Range.clip(y + x + r, -1, 1));
-		backRight.setPower(Range.clip(y - x + r, -1, 1));
 		
 	}
 	
 	private void runLift() {
 		
-		
+		if(gamepad1.right_trigger != 0 || gamepad1.left_trigger != 0 ||
+				gamepad2.right_trigger != 0 || gamepad2.left_trigger != 0 ||
+				gamepad1.right_bumper || gamepad1.left_bumper ||
+				gamepad2.right_bumper || gamepad2.right_bumper) {
+			grabbingStack = false;
+		}
 		
 		double liftPower;
 		
 		//Operate the lift when the triggers are pressed.
 		if(gamepad1.right_trigger != 0) { //First right trigger - raises lift.
 			
-			liftPower = gamepad1.right_trigger * .75;
+			liftPower = gamepad1.right_trigger * 1;
 			
 		} else if(gamepad1.left_trigger != 0) { //First left trigger - lowers lift.
 			
-			liftPower = -gamepad1.left_trigger * .75;
+			liftPower = -gamepad1.left_trigger * 1;
 			
 		} else if(gamepad2.right_trigger != 0) { //Second right trigger - raises lift.
 			
-			liftPower = gamepad2.right_trigger * .75;
+			liftPower = gamepad2.right_trigger * 1;
 			
 		} else if(gamepad2.left_trigger != 0) { //Second left trigger - lowers lift.
 			
-			liftPower = -gamepad2.left_trigger * .75;
+			liftPower = -gamepad2.left_trigger * 1;
 			
 		} else { //Deactivates lift.
 			
@@ -224,9 +372,9 @@ public class TeleOp_v2 extends OpMode {
 			if(gamepad1.left_bumper || gamepad2.left_bumper) {
 				
 				
-				if(leftLift.getCurrentPosition() > 200) {
+				if (leftLift.getCurrentPosition() > 200) {
 					liftPower = -.5;
-				} else if(leftLift.getCurrentPosition() > 0) {
+				} else if (leftLift.getCurrentPosition() > 0) {
 					liftPower = -.1;
 				}
 				
@@ -234,14 +382,19 @@ public class TeleOp_v2 extends OpMode {
 			
 		}
 		
-		
-		leftLift.setPower(liftPower);
-		rightLift.setPower(liftPower);
-		
+		if(!grabbingStack) {
+			leftLift.setPower(liftPower);
+			rightLift.setPower(liftPower);
+		}
 		
 	}
 	
 	private void runGripper() {
+		
+		if(gamepad1.a || gamepad1.b || gamepad1.x || gamepad1.y ||
+				gamepad2.a || gamepad2.b || gamepad2.x || gamepad2.y) {
+			grabbingStack = false;
+		}
 		
 		// --- Lower gripper ---
 		if((gamepad1.a || gamepad2.a) && !gripperPressedLast) {
